d85809c14643ae1bfd810827d5b6ab26a839ea1d
YOULYU ZHANG <youlyuzhang.work@gmail.com>
2018-06-04 08:56:18 +0900

    valid sudoku added.

--- src/main/java/leetcode/valid_sudoku_0036/Solution.java
+++ src/main/java/leetcode/valid_sudoku_0036/Solution.java
@@ -1,26 +1,26 @@
-public class ValidSudoku {
-    public static final boolean DEV_MODE = true;
+package leetcode.valid_sudoku_0036;
+
+//import java.util.logging.Logger;
+
+class Solution {
+//    private Logger LOGGER = Logger.getLogger(this.getClass().getName());
 
     public boolean isValidSudoku(char[][] board) {
-        if (DEV_MODE) System.out.println("Row, Column,Grid");
+//        LOGGER.info("Row, Column,Grid");
         for (int i = 0; i < 9; i++) {
             boolean[] usedInGrid = new boolean[9];
             boolean[] usedInRow = new boolean[9];
             boolean[] usedInColumn = new boolean[9];
-            if (DEV_MODE) System.out.println("----------------");
             for (int j = 0; j < 9; j++) {
 
                 int valueInRow = board[i][j] == '.' ? -1 : board[i][j] - '1';
                 int valueInColumn = board[j][i] == '.' ? -1 : board[j][i] - '1';
                 char charInGrid = board[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3];
                 int valueInGrid = charInGrid == '.' ? -1 : charInGrid - '1';
-
-                if (DEV_MODE) {
-                    System.out.println("(" + i + "," + j + "," + board[i][j] + ")" +
-                            "(" + j + "," + i + "," + board[j][i] + ")" +
-                            "(" + ((i / 3) * 3 + j / 3) + "," + ((i % 3) * 3 + j % 3) + "," +
-                            board[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3] + ")");
-                }
+//                LOGGER.info("(" + i + "," + j + "," + board[i][j] + ")" +
+//                        "(" + j + "," + i + "," + board[j][i] + ")" +
+//                        "(" + ((i / 3) * 3 + j / 3) + "," + ((i % 3) * 3 + j % 3) + "," +
+//                        board[(i / 3) * 3 + j / 3][(i % 3) * 3 + j % 3] + ")");
 
                 if (valueInRow != -1) {
                     if (usedInRow[valueInRow]) return false;
