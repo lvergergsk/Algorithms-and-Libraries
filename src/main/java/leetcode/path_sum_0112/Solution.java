7090643eaea767054036e62de576c09d44689afd
YOULYU ZHANG <youlyuzhang.work@gmail.com>
2018-06-14 10:43:02 +0900

    document added.

---
+++ src/main/java/leetcode/path_sum_0112/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.path_sum_0112;
+
+
+//  Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return false;
+    }
+}
